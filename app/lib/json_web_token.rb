74714b3a16a005f28887cccf829242d4831ccf69
agustorrano <agustorrano2003@gmail.com>
2025-05-13 11:47:25 -0300

    Secretos

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -6,16 +6,16 @@ class JsonWebToken
     JWT.decode(token, nil,
                true,
                algorithm: "RS256",
-               iss: "https://dev-q0d4vav2ugbbqjjz.us.auth0.com/",
+               iss: "https://#{Rails.application.credentials.auth0[:domain]}/",
                verify_iss: true,
-               aud: "Cit7wosBRhGh1jTLLMjADaOxMG3eiRlw",
+               aud: Rails.application.credentials.auth0[:client_id],
                verify_aud: true) do |header|
       jwks_hash[header["kid"]]
     end
   end
 
   def self.jwks_hash
-    jwks_raw = Net::HTTP.get URI("https://dev-q0d4vav2ugbbqjjz.us.auth0.com/.well-known/jwks.json")
+    jwks_raw = Net::HTTP.get URI("https://#{Rails.application.credentials.auth0[:domain]}/.well-known/jwks.json")
     jwks_keys = Array(JSON.parse(jwks_raw)["keys"])
 
     Hash[
